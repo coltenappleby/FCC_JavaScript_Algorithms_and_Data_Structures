cce4f616d8698f297c484c01547992aec02703ca
Colten Appleby <colten.appleby@gmail.com>
2021-06-24 21:45:41 -0400

    testing using mark up langauge instead of JS files

---
+++ Basic-JavaScript.js
@@ -0,0 +1,35 @@
+// ##################################################################
+// ########################### Variables ############################
+// ##################################################################
+
+// Initializing Variables with the Assignment
+
+// var a = 9;
+
+// if a variable is declared but is not assigned it is undefined and if you do a mathematical operation on it then it will result in NaN (Not a Number)
+
+var a; //undefined
+
+// variable and function names are case-sensitive 
+
+// MYVAR and myvar are different variables
+// in JavaScript we use camelCase!
+
+
+// Incrementing a number
+let i = 0
+i++
+//decrementing a variable
+let j = 0
+j--
+
+
+// Remainder operator
+
+5 % 2 // = 1
+// because 
+Math.floor ( 5 / 2 ) = 2
+// 2 * 2 // = 4
+// 5 - 4 // = 1
+// Great to check if a number is even or odd
+
